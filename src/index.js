72d6baa63f4676daec0033e09ffba6d742ff2e94
MinecraftArchive <classicpackteam@gmail.com>
2018-07-02 20:55:35 -0400

    take whaTEVER

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+// Open Port
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+}
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}
+
 // Verify NodeJS version
 const nodeMajorVersion = parseInt(process.versions.node.split('.')[0], 10);
 if (nodeMajorVersion < 8) {
