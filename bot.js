005af7581bac4174ff7183cd6545d2061f0fef13
MinecraftArchive <classicpackteam@gmail.com>
2018-07-02 21:09:02 -0400

    fINALLY.

--- bot.js
+++ bot.js
@@ -1,2 +1,6 @@
 const config = require('./src/config');
 module.exports = config.knex;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
